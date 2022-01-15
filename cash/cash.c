174bbef1d8093d61dba1448788bb6784dafc4c58
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-15 09:49:15 +0000

    Sat, Jan 15, 2022, 9:49 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -10,8 +10,10 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    int cents = get_cents();
-
-
+    do
+    {
+     int cents = get_cents();
+    }
+    while (cents < 0)
 
 }
