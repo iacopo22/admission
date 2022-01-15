c855b3beb59c4bad12a16aa7d5229cb2a43970bb
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-15 20:19:32 +0000

    Sat, Jan 15, 2022, 8:19 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -13,75 +13,75 @@ int calculate_pennies(int cents);
 //Define my function int get_cents
 int get_cents(void)
 {
-  int cents;
-   do
-   {
-     cents = get_int("Cents owed: ");
-  }
-   while (cents < 0);
+    int cents;
+    do
+    {
+        cents = get_int("Cents owed: ");
+    }
+    while (cents < 0);
     return cents;
 }
 
 
-    // Calculate the number of quarters to give the customer
+// Calculate the number of quarters to give the customer
 
-    //Define my function int calculate_quarters
-     int quarters;
-    int calculate_quarters(int cents)
+//Define my function int calculate_quarters
+int quarters;
+int calculate_quarters(int cents)
+{
+    for (quarters = 0; cents >= 25; quarters++)
     {
-      for(quarters=0; cents>=25; quarters++)
-      {
-      cents = cents - 25;
-      }
-      return quarters;
+        cents = cents - 25;
     }
+    return quarters;
+}
 
 
-    // Calculate the number of dimes to give the customer
+// Calculate the number of dimes to give the customer
 
-    //Define my function int calculate_dimes
-    int dimes;
-    int calculate_dimes(int cents)
+//Define my function int calculate_dimes
+int dimes;
+int calculate_dimes(int cents)
+{
+    for (dimes = 0; cents >= 10; dimes++)
     {
-      for(dimes=0; cents>=10; dimes++)
-      {
         cents = cents - 10;
-      }
-      return dimes;
     }
+    return dimes;
+}
 
 
-    // Calculate the number of nickels to give the customer
+// Calculate the number of nickels to give the customer
 
-    //Define my function int calculate_ nickels
-    int nickels;
-    int calculate_nickels(int cents)
+//Define my function int calculate_ nickels
+int nickels;
+int calculate_nickels(int cents)
+{
+    for (nickels = 0; cents >= 5; nickels++)
     {
-      for(nickels=0; cents>=5; nickels++)
-      {
         cents = cents - 5;
-      }
-      return nickels;
     }
+    return nickels;
+}
 
-    // Calculate the number of pennies to give the customer
-    int pennies;
-    int calculate_pennies(int cents)
+// Calculate the number of pennies to give the customer
+int pennies;
+int calculate_pennies(int cents)
+{
+    for (pennies = 0; cents >= 1; pennies++)
     {
-      for(pennies=0; cents>=1; pennies++)
-      {
         cents = cents - 1;
-      }
-      return pennies;
     }
+    return pennies;
+}
 
 
-    int main(void)
+int main(void)
 {
 
     //Recall my function int get_cents
     int cents;
-     cents = get_cents();
+    cents = get_cents();
 
     //Recall my function int calculate_quarters
     quarters = calculate_quarters(cents);
