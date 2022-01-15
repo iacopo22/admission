5c70229e42bb14d3b1f3a552b6ea23866d27d290
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-15 18:19:50 +0000

    Sat, Jan 15, 2022, 6:19 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -29,19 +29,22 @@ int calculate_pennies(int cents);
     //Define my function int calculate_quarters
     int calculate_quarters(int cents)
     {
-      int quarters;
-      
+      int numberq;
+
       for(numberq=0; cents>=25; numberq++)
-      return quarters;
+      cents = cents - 25;
+      return numberq;
     }
 
 
     int main(void)
 {
     //Recall my function int get_cents
+    int cents;
      cents = get_cents();
 
     //Recall my function int calculate_quarters
+    int numberq;
     int quarters = calculate_quarters(cents);
     cents = cents - quarters * 25;
     printf("%i", numberq);
