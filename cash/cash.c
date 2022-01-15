822b77a4684e3c701a4cdce842ac783c233e1b07
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-15 09:45:58 +0000

    Sat, Jan 15, 2022, 9:45 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -12,55 +12,6 @@ int main(void)
     // Ask how many cents the customer is owed
     int cents = get_cents();
 
-    // Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
 
-    // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
 
-    // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
-
-    // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
-
-    // Sum coins
-    int coins = quarters + dimes + nickels + pennies;
-
-    // Print total number of coins to give the customer
-    printf("%i\n", coins);
-}
-
-int get_cents(void)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_quarters(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_dimes(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_nickels(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_pennies(int cents)
-{
-    // TODO
-    return 0;
 }
