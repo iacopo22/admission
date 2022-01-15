d274773afa91031dc5ee94c861e637be906571cd
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-15 11:04:02 +0000

    Sat, Jan 15, 2022, 11:04 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
+int get_cents(void);
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
@@ -9,16 +9,13 @@ int calculate_pennies(int cents);
 
 int main(void)
 {
+    int cents;
     // Ask how many cents the customer is owed
     int get_cents(void);
-    {
     do
     {
-      cents = get_cents(int);
+      cents = get_cents("Cents: ");
     }
-    while (cents < 0);
+    while ( cents < 0);
     return cents;
-    }
-
-
 }
