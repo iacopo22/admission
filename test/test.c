5154206e43c106b6be67fc7dd261ca31158bc9dc
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 11:07:25 +0000

    Sun, Jan 16, 2022, 11:07 AM +00:00

--- test/test.c
+++ test/test.c
@@ -1,8 +1,17 @@
+// Prints a row of n question marks with a loop
+
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    for (int i = 0; i < 4; i++)
+    int n;
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n < 1);
+    for (int i = 0; i < n; i++)
     {
         printf("?");
     }
