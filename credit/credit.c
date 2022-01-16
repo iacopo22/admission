ac6f285df7f34a7e558949259788fb069da77bc1
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 18:13:33 +0000

    Sun, Jan 16, 2022, 6:13 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -20,7 +20,7 @@ int main(void)
 int alternative_digits(long int n)
 {
 	int y;
-	for(y = 3; (n/10^y)>=0.1; y = y + 2)
+	for(y = 3; (n/10^y)>=(0.1); y = y + 2)
 	{
 		int j = (n % 10^(y)/10^(y - 1)) - (n % 10^(y - 1))/(10^(y - 1));
 	}
