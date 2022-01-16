ae8bfdf1003d7b20dcb14e989adf4464068555d3
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 18:49:02 +0000

    Sun, Jan 16, 2022, 6:49 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,28 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 // Initialize my functions
-int sum_alternative_digits(long int n);
+int sum_alternative_digits(double int n);
 
 
 
 int main(void)
 {
-	long int n = get_long("Numbers: ");
+	double int n = get_long("Numbers: ");
 
-	int z = (n % 10) + alternative_digits(n);
+	int z = (n % 10) + sum_alternative_digits(n);
 
 	printf("%i", z);
 
 }
 
 
-int alternative_digits(long int n)
+int sum_alternative_digits(double int n)
 {
-	long int y;
-	for(y = 3; (n/10^y)>=(0.1); y = y + 2)
+	double int y;
+	for(y = 3; (n/pow(10, y))>=(0.1); y = y + 2)
 	{
-		int j = (n % 10^(y)/10^(y - 1)) - (n % 10^(y - 1))/(10^(y - 1));
+		int j = ((n % pow(10,y))/pow(10,(y - 1))) - ((n % pow(10,(y - 1)))/(pow(10,(y - 1)));
 	}
 	return 1;
 }
