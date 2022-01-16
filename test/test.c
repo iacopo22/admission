fa1cedebc07754aa86e581ec3996c2dc46e5ca78
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 11:01:27 +0000

    Sun, Jan 16, 2022, 11:01 AM +00:00

--- test/test.c
+++ test/test.c
@@ -1,19 +1,19 @@
-// Return value
+// Return value, multiple arguments
 
 #include <cs50.h>
 #include <stdio.h>
 
-float discount(float price);
+float discount(float price, int percentage);
 
 int main(void)
 {
-    float regular = get_float("Regular Price: ");
-    float sale = discount(regular);
-    printf("Sale Price: %.2f\n", sale);
+    float regular_price = get_float("Regular Price: ");
+    int percent_off = get_int("Percent Off: ");
+    printf("Sale Price: %.2f\n", discount(regular_price, percent_off));
 }
 
 // Discount price
-float discount(float price)
+float discount(float price, int percentage)
 {
-    return price * .85;
+    return price * (100 - percentage) / 100;
 }
