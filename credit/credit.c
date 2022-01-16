bf6c49955b3d88135eb1b6608aa02805e22d8005
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 17:07:43 +0000

    Sun, Jan 16, 2022, 5:07 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,12 +8,17 @@ int alternative_digits(int digits);
 
 int main(void)
 {
-	int y;
 	long int n = get_long("Numbers: ");
-
-	for(y = 2; (n/10^y)>=1; y++)
-	{
-		(n % 10) - ((n % 10^y - n % 10^y)/10^y);
-	}
+	int z
 
 }
+
+
+int alternative_digits(int digits)
+{
+	for(y = 2; (n/10^y)>=1; y++)
+	{
+		((n % 10^y - n % 10^y)/10^y);
+	}
+}
+
