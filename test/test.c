a61ecbd96a9840624339c8876571e81cfcf67483
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 10:48:32 +0000

    Sun, Jan 16, 2022, 10:48 AM +00:00

--- test/test.c
+++ test/test.c
@@ -1,11 +1,19 @@
-// No return value
+// Return value
 
 #include <cs50.h>
 #include <stdio.h>
 
+float discount(float price);
+
 int main(void)
 {
     float regular = get_float("Regular Price: ");
-    float sale = regular * .85;
+    float sale = discount(regular);
     printf("Sale Price: %.2f\n", sale);
+}
+
+// Discount price
+float discount(float price)
+{
+    return price * .85;
 }
