41183a8219b41dd7bcb5ec9fa9cb8649e71d8434
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 11:13:14 +0000

    Sun, Jan 16, 2022, 11:13 AM +00:00

--- test/test.c
+++ test/test.c
@@ -1,11 +1,19 @@
-// Better design
+// Abstraction
 
 #include <stdio.h>
+
+void meow(void);
 
 int main(void)
 {
     for (int i = 0; i < 3; i++)
     {
-        printf("meow\n");
+        meow();
     }
+}
+
+// Meow once
+void meow(void)
+{
+    printf("meow\n");
 }
