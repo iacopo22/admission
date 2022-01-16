9105584d6f301395a42095c1a493f18b4fd0530b
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 17:14:11 +0000

    Sun, Jan 16, 2022, 5:14 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 // Initialize my functions
-int alternative_digits(int digits);
+int alternative_digits(long int n);
 
 
 
@@ -10,16 +10,17 @@ int main(void)
 {
 	long int n = get_long("Numbers: ");
 
-	int z = (n % 10) + alternative_digits(n)
+	int z = (n % 10) + alternative_digits(n);
 
 }
 
 
-int alternative_digits(int digits)
+int alternative_digits(long int n)
 {
+	int y;
 	for(y = 2; (n/10^y)>=1; y++)
 	{
-		((n % 10^y - n % 10^y)/10^y);
+		((n % 10^(y) - n % 10^(y))/10^(y));
 	}
 }
 
