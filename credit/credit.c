549af9a0ab0a54dd799390041880879c98af98e4
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-16 17:03:52 +0000

    Sun, Jan 16, 2022, 5:03 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,25 +8,12 @@ int alternative_digits(int digits);
 
 int main(void)
 {
-	// Program receive numbers of the card from the user
-	
-
-
-	int z = alternative_digits(n);
-	printf("%i", z);
-
-}
-
-	// Verify the sum of alternative digits
 	int y;
 	long int n = get_long("Numbers: ");
-	int alternative_digits(int digits)
+
+	for(y = 2; (n/10^y)>=1; y++)
 	{
-		for(y = 2; (n/10^y)>=1; y++)
-		{
-			(n % 10) - ((n % 10^y - n % 10^y)/10^y);
-		}
+		(n % 10) - ((n % 10^y - n % 10^y)/10^y);
 	}
-	return digits;
 
-
+}
