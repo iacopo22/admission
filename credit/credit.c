2634ef7e5422309e5e0e6fb86f3c731683d4fce2
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-17 18:14:16 +0000

    Mon, Jan 17, 2022, 6:14 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -21,10 +21,15 @@ int main(void)
 		c = (x / pow(10, y));
 		z = c % 10;
 		printf("%li\n", z);
-		if(y==1 || y==3 || y==5 || y==7 || y==9 || y==11 || y==13 || y==15)
+		
+		if(y==1)
 		{
-			long sum_secondtolast = z;
+			long secondtolast = z;
 			printf("Second to last: %li\n", sum_secondtolast);
+		}
+		else(y==3)
+		{
+
 		}
 
 	}
