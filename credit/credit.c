5e4e8fb796d9d3f169898c46a2aeeefc40d5be2c
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-17 10:16:11 +0000

    Mon, Jan 17, 2022, 10:16 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,29 +2,13 @@
 #include <stdio.h>
 #include <math.h>
 
-// Initialize my functions
-int sum_alternative_digits(long int n);
 
 
 
 int main(void)
 {
+	// Get the number of the card from the user
 	long int n = get_long("Numbers: ");
 
-	int z = (n % 10) + sum_alternative_digits(n);
-
-	printf("%i", z);
 
 }
-
-
-int sum_alternative_digits(long int n)
-{
-	long int y;
-	for(y = 3; (n/pow(10, y))>=(0.1); y = y + 2)
-	{
-		int j = ((n % pow(10,y))/pow(10,(y - 1))) - ((n % pow(10,(y - 1)))/(pow(10,(y - 1)));
-	}
-	return 1;
-}
-
