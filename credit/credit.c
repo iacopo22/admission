84aca99c2dad030da1f2da36d489d6c9c55d8afe
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-17 12:17:19 +0000

    Mon, Jan 17, 2022, 12:17 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,13 +11,11 @@ int main(void)
 	long int ncard = get_long("Number of the card: ");
 	long int last_digit = (ncard / 1) % 10;
 
+	
 
-	if
-	{
-		last_digit = (ncard / 10) % 10;
-		printf("%ld\n", secondlat_digit);
-	}
-	else
+
+
+
 
 
 }
