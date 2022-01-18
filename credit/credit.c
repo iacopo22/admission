65a899f50e403862108515473060d16947add429
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-18 11:44:27 +0000

    Tue, Jan 18, 2022, 11:44 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -39,94 +39,94 @@ int main(void)
 
 		switch(y)
 		{
-		case 0: (y = 0);
+		case 0:
 			last = z;
 			printf("Last: %li\n", last);
 			break;
 
-		case 1: (y = 1);
+		case 1:
 			secondtolast = z;
 			printf("Second to last: %li\n", secondtolast);
 			break;
 
-		case 2: (y = 2);
+		case 2:
 			thirdtolast = z;
 			printf("Third to last: %li\n", thirdtolast);
 			break;
 
-		case 3: (y = 3);
+		case 3:
 
 			fourthtolast = z;
 			printf("Fourth to last: %li\n", fourthtolast);
 			break;
 
-		case 4: (y = 4);
+		case 4:
 
 			fifthtolast = z;
 			printf("Fifth to last: %li\n", fifthtolast);
 			break;
 
-		case 5: (y = 5);
+		case 5:
 
 			sixthtolast = z;
 			printf("Sixth to last: %li\n", sixthtolast);
 			break;
 
-		case 6: (y = 6);
+		case 6:
 
 			seventhtolast = z;
 			printf("Seventh to last: %li\n", seventhtolast);
 			break;
 
-		case 7: (y = 7);
+		case 7:
 
 			eighthtolast = z;
 			printf("Eighth to last: %li\n", eighthtolast);
 			break;
 
-		case 8: (y = 8);
+		case 8:
 
 			ninthtolast = z;
 			printf("Ninth to last: %li\n", ninthtolast);
 			break;
 
-		case 9: (y = 9);
+		case 9:
 
 			tenthtolast = z;
 			printf("Tenth to last: %li\n", tenthtolast);
 			break;
 
-		case 10: (y = 10);
+		case 10:
 
 			 sixth = z;
 			printf("Sixth: %li\n", sixth);
 			break;
 
-		case 11: (y = 11);
+		case 11:
 
 			fifth = z;
 			printf("Fifth: %li\n", fifth);
 			break;
 
-		case 12: (y = 12);
+		case 12:
 
 			fourth = z;
 			printf("Fourth: %li\n", fourth);
 			break;
 
-		case 13: (y = 13);
+		case 13:
 
 			third = z;
 			printf("Third: %li\n", third);
 			break;
 
-		case 14: (y = 14);
+		case 14:
 
 			second = z;
 			printf("Second: %li\n", second);
 			break;
 
-		case 15: (y = 15);
+		case 15:
 
 			first = z;
 			printf("First: %li\n", first);
