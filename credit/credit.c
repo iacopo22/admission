0f4f4884e754e91a94f7c3816ec348d6c3dbaf8b
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-18 19:22:12 +0000

    Tue, Jan 18, 2022, 7:22 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -36,176 +36,173 @@ int main(void)
 		c = (ncard / pow(10, y));
 		z = c % 10;
 
-	switch (y)
-	{
-		case 0:
-			last = z;
-		break;
-
-		case 1:
-			secondtolast = z;
-			if((secondtolast * 2) >= 10)
-			{
-				_secondtolast = (secondtolast * 2) % 10;
-				__secondtolast = ((secondtolast * 2) / 10) % 10;
-				secondtolast = 0;
-			}
-			else
-			{
-				_secondtolast = 0;
-				__secondtolast = 0;
-			}
-		break;
-
-		case 2:
-			thirdtolast = z;
-		break;
-
-		case 3:
-
-			fourthtolast = z;
-			if ((fourthtolast * 2) >= 10)
-			{
-				_fourthtolast = (fourthtolast * 2) % 10;
-				__fourthtolast = ((fourthtolast * 2) / 10) % 10;
-				fourthtolast = 0;
-			}
-			else
-			{
-				_fourthtolast = 0;
-				__fourthtolast = 0;
-			}
-		break;
-
-		case 4:
-
-			fifthtolast = z;
-		break;
-
-		case 5:
-
-			sixthtolast = z;
-			if((sixthtolast * 2) >= 10)
-			{
-				_sixthtolast = (sixthtolast * 2) % 10;
-				__sixthtolast = ((sixthtolast * 2) / 10) % 10;
-				sixthtolast = 0;
-			}
-			else
-			{
-				_sixthtolast = 0;
-				__sixthtolast = 0;
-			}
-		break;
-
-		case 6:
-
-			seventhtolast = z;
-		break;
-
-		case 7:
-
-			eighthtolast = z;
-			if((eighthtolast * 2) >= 10)
-			{
-				_eighthtolast = (eighthtolast * 2 ) % 10;
-				__eighthtolast = ((eighthtolast * 2) / 10) % 10;
-				eighthtolast = 0;
-			}
-			else
-			{
-				_eighthtolast = 0;
-				__eighthtolast = 0;
-			}
-		break;
-
-    	case 8:
-
-			ninthtolast = z;
-		break;
-
-		case 9:
-
-			tenthtolast = z;
-			if((tenthtolast*2)>=10)
-			{
-				_tenthtolast = (tenthtolast * 2) % 10;
-				__tenthtolast= ((tenthtolast * 2)/ 10) % 10;
-				tenthtolast = 0;
-			}
-			else
-			{
-				_tenthtolast = 0;
-				__tenthtolast = 0;
-			}
-		break;
-
-
-		case 10:
-
-		 	sixth = z;
-		break;
-
-	case 11:
-
-		fifth = z;
-		if((fifth * 2) >= 10)
+		switch (y)
 		{
-			_fifth = (fifth * 2) % 10;
-			__fifth = ((fifth * 2) / 10) % 10;
-			fifth = 0;
-		}
-		else
-		{
-			_fifth = 0;
-			__fifth = 0;
-				printf("Fifth: %li\n", fifth);
-		}
-		break;
+			case 0:
+				last = z;
+				break;
 
-    	case 12:
+			case 1:
+				secondtolast = z;
+				if((secondtolast * 2) >= 10)
+				{
+					_secondtolast = (secondtolast * 2) % 10;
+					__secondtolast = ((secondtolast * 2) / 10) % 10;
+					secondtolast = 0;
+				}
+				else
+				{
+					_secondtolast = 0;
+					__secondtolast = 0;
+				}
+				break;
 
-			fourth = z;
-		printf("Fourth: %li\n", fourth);
-		break;
+			case 2:
+				thirdtolast = z;
+				break;
 
-    case 13:
+			case 3:
 
-		third = z;
-		if((third * 2) >= 10)
-		{
-			_third = (third * 2) % 10;
-			__third = ((third * 2) / 10) % 10;
-			third = 0;
-		}
-		else
-		{
-			_third = 0;
-			__third = 0;
-		}
-		break;
+				fourthtolast = z;
+				if ((fourthtolast * 2) >= 10)
+				{
+					_fourthtolast = (fourthtolast * 2) % 10;
+					__fourthtolast = ((fourthtolast * 2) / 10) % 10;
+					fourthtolast = 0;
+				}
+				else
+				{
+					_fourthtolast = 0;
+					__fourthtolast = 0;
+				}
+				break;
 
-    case 14:
+			case 4:
 
-		second = z;
-		printf("Second: %li\n", second);
-		break;
+				fifthtolast = z;
+				break;
 
-    case 15:
+			case 5:
 
-    first = z;
-	if((first*2) >= 10)
-	{
-		_first = (first * 2) % 10;
-			__first = ((first * 2) / 10) % 10;
-			first = 0;
-		}
-		else
-		{
-       _first = 0;
-       __first = 0;
-     }
-             break;
+				sixthtolast = z;
+				if((sixthtolast * 2) >= 10)
+				{
+					_sixthtolast = (sixthtolast * 2) % 10;
+					__sixthtolast = ((sixthtolast * 2) / 10) % 10;
+					sixthtolast = 0;
+				}
+				else
+				{
+					_sixthtolast = 0;
+					__sixthtolast = 0;
+				}
+				break;
+
+			case 6:
+
+				seventhtolast = z;
+				break;
+
+			case 7:
+
+				eighthtolast = z;
+				if((eighthtolast * 2) >= 10)
+				{
+					_eighthtolast = (eighthtolast * 2 ) % 10;
+					__eighthtolast = ((eighthtolast * 2) / 10) % 10;
+					eighthtolast = 0;
+				}
+				else
+				{
+					_eighthtolast = 0;
+					__eighthtolast = 0;
+				}
+				break;
+
+	    	case 8:
+
+				ninthtolast = z;
+				break;
+
+			case 9:
+
+				tenthtolast = z;
+				if((tenthtolast*2)>=10)
+				{
+					_tenthtolast = (tenthtolast * 2) % 10;
+					__tenthtolast= ((tenthtolast * 2)/ 10) % 10;
+					tenthtolast = 0;
+				}
+				else
+				{
+					_tenthtolast = 0;
+					__tenthtolast = 0;
+				}
+				break;
+
+
+			case 10:
+
+			 	sixth = z;
+				break;
+
+			case 11:
+
+				fifth = z;
+				if((fifth * 2) >= 10)
+				{
+					_fifth = (fifth * 2) % 10;
+					__fifth = ((fifth * 2) / 10) % 10;
+					fifth = 0;
+				}
+				else
+				{
+					_fifth = 0;
+					__fifth = 0;
+				}
+				break;
+
+    		case 12:
+
+				fourth = z;
+				break;
+
+    		case 13:
+
+				third = z;
+				if((third * 2) >= 10)
+				{
+					_third = (third * 2) % 10;
+					__third = ((third * 2) / 10) % 10;
+					third = 0;
+				}
+				else
+				{
+					_third = 0;
+					__third = 0;
+				}
+				break;
+
+    		case 14:
+
+				second = z;
+				break;
+
+    		case 15:
+
+    			first = z;
+				if((first*2) >= 10)
+				{
+					_first = (first * 2) % 10;
+					__first = ((first * 2) / 10) % 10;
+					first = 0;
+				}
+				else
+				{
+       				_first = 0;
+       				__first = 0;
+     			}
+				break;
         }
     }
 
@@ -215,33 +212,30 @@ int main(void)
 	                           + _tenthtolast + __tenthtolast + eighthtolast * 2 + _eighthtolast + __eighthtolast + sixthtolast * 2 + _sixthtolast
                                + __sixthtolast + fourthtolast * 2 + _fourthtolast + __fourthtolast + secondtolast * 2 + _secondtolast + __secondtolast;
 
-    printf("Sum of alternative digits from second one: %i\n", sum_from_secondtolast);
 
     int sum = sum_from_secondtolast + sum_from_the_last;
-    printf("Sum: %i\n", sum);
 
     int last_sum = sum % 10;
-    printf("Is a zero? %i\n", last_sum);
 
 
 
-    if( pow(10, 14) <= ncard <= 5 * (pow(10, 14)) && (last_sum == 0) && (second == 3) && ((_third == 4 && __third == 1) || third == 4))
-    {
-        printf("AMEX\n");
-    }
-    else if( pow(10, 15) <= ncard <= 6 * (pow(10, 15)) && (last_sum == 0) && (second == 1 || second == 2 || second == 3 || second == 4
-               || second == 5) && (_first == 0 && __first == 1))
-    {
-        printf("MASTERCARD\n");
-    }
-   else if( pow(10, 12) <= ncard <= 5 * (pow(10, 15)) && (last_sum == 0) && (first == 4 || second == 4 || third == 4 || fourth == 4))
-   {
-       printf("VISA\n");
-   }
-     else
-     {
-        printf("INVALID\n");
-     }
+	if( pow(10, 14) <= ncard <= 5 * (pow(10, 14)) && (last_sum == 0) && (second == 3) && ((_third == 4 && __third == 1) || third == 4))
+	{
+		printf("AMEX\n");
+	}
+	else if( pow(10, 15) <= ncard <= 6 * (pow(10, 15)) && (last_sum == 0) && (second == 1 || second == 2 || second == 3 || second == 4
+            || second == 5) && (_first == 0 && __first == 1))
+	{
+		printf("MASTERCARD\n");
+	}
+	else if( pow(10, 12) <= ncard <= 5 * (pow(10, 15)) && (last_sum == 0) && (first == 4 || second == 4 || third == 4 || fourth == 4))
+	{
+		printf("VISA\n");
+	}
+	else
+	{
+		printf("INVALID\n");
+	}
 
 
 
