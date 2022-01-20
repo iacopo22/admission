32d18b31e0d3913ecbd294c39498691ce4938520
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-20 11:55:13 +0000

    Thu, Jan 20, 2022, 11:55 AM +00:00

--- credit/test.c
+++ credit/test.c
@@ -1,15 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int triangle(int x, int y, int z);
+int triangle(int a, int b, int c);
 
 int main(void)
 {
+	int x = get_int("One of the side: ");
+	int y = get_int("One of the side: ");
+	int z = get_int("One of the side: ");
+
+	
 
 }
 
 
 int triangle(int x, int y, int z)
 {
-	if()
+	if(a + b > c || a + c > b || c + b > a)
+	{
+
+	}
 }
