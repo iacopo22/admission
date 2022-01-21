aef2faa0629abff86a5d24ded5522ee9e15ec631
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-21 11:45:17 +0000

    Fri, Jan 21, 2022, 11:45 AM +00:00

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int loop_elements(void);
+
+int main(void)
+
+{
+	
+	int test[loop_elements];
+}
+
+int loop_elements(void)
+{
+	for(i = 0; i < 100; i++)
+	{
+		return i;
+	}
+}
