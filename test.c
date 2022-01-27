d85905a48d05bf622ca1d580052e007c19fecb32
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-27 11:50:06 +0000

    Thu, Jan 27, 2022, 11:50 AM +00:00

--- test.c
+++ test.c
@@ -6,9 +6,11 @@ int main(void)
 {
     string s = get_string("Input:  ");
     printf("Output: ");
-    for (int i = 0; i < strlen(s); i++)
+	int n = strlen(s);
+    for (int i = 0; i < n; i++)
     {
         printf("%c", s[i]);
     }
-    printf("\n");
+	printf("\n");
+	printf("%i\n", n);
 }
