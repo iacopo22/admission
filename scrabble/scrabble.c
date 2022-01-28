7b7b7787e6f4ddd9b83540d12d93ea2a8de52fdc
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-01-28 10:43:48 +0000

    Fri, Jan 28, 2022, 10:43 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -23,6 +23,7 @@ int main(void)
 
 int compute_score(string word)
 {
+	char a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
 	int CHAR[] = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
 	for(int j = 0; j < 26; j++)
 	{
