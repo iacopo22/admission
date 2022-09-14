3594c07cd297293ded5dfa6d04462c6b12446e2a
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-14 07:42:38 +0000

    Wed, Sep 14, 2022, 7:42 AM +00:00

--- triangle.c
+++ triangle.c
@@ -9,7 +9,7 @@ int main(void)
 	int l2 = get_int("Length of one side: ");
 	int l3 = get_int("Length of one side: ");
 
-	bool valid_triangle;
+	bool valid_triangle(int l1, int l2, int l3);
 }
 
 bool valid_triangle(int l1, int l2, int l3)
