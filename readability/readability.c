758c61d5c19a806bf57a7dc21060df1c1c6ce305
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-14 18:28:13 +0000

    Wed, Sep 14, 2022, 6:28 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,25 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//I declare the function
 int count_letters(string text);
 
 int main(void)
 {
-	//I want the user to prompt 
+	//I want the user to prompt a string, not numbers or symbols
 	do
 	{
-	string text = get_string("Text: ");
+		string text = get_string("Text: ");
 	}
 	while(text == %i);
 
+	//Here the program prints the text
 	printf("Text: %s\n", text);
 
+	//I recall the function count_letters
 	int count_letters(string text);
 	printf("Number of letters: %i\n", count_letters);
 
 }
 
-
+//I define the function
 int count_letters(string text)
 {
 
