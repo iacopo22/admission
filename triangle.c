0e7fe13cb69998b96830b89d11cd70f2848a6179
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-14 07:57:11 +0000

    Wed, Sep 14, 2022, 7:57 AM +00:00

--- triangle.c
+++ triangle.c
@@ -17,11 +17,12 @@ bool valid_triangle(int l1, int l2, int l3)
 
 	if (l1+l2>l3 && l2+l3>l1 && l1+l3>l2 && l1>0 && l2>0 && l3>0)
 	{
+		printf("NGL fucking genius\n");
 		return true;
-		printf("NGL fucking genius");
     }
 	else
 	{
+		printf("NGL not genius\n");
 		return false;
 	}
 
