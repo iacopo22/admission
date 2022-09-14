9134d38036e4950677a6e688a6f18289e0b4a157
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-14 07:36:52 +0000

    Wed, Sep 14, 2022, 7:36 AM +00:00

--- triangle.c
+++ triangle.c
@@ -11,7 +11,7 @@ int main(void)
 bool valid_triangle(int l1, int l2, int l3)
 {
 
-	if (l1+l2>l3, l2+l3>l1, l1+l3>l2 && l1>0, l2>0, l3>0)
+	if ((l1+l2>l3 || l2+l3>l1 || l1+l3>l2) && (l1>0 && l2>0 && l3>0))
 	{
 		return 0;
     }
