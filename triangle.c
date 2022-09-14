524b8883687635efe409c359dccbd44f4f53180f
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-14 07:41:42 +0000

    Wed, Sep 14, 2022, 7:41 AM +00:00

--- triangle.c
+++ triangle.c
@@ -5,6 +5,10 @@ bool valid_triangle(int x, int y, int z);
 
 int main(void)
 {
+	int l1 = get_int("Length of one side: ");
+	int l2 = get_int("Length of one side: ");
+	int l3 = get_int("Length of one side: ");
+
 	bool valid_triangle;
 }
 
@@ -13,11 +17,13 @@ bool valid_triangle(int l1, int l2, int l3)
 
 	if ((l1+l2>l3 || l2+l3>l1 || l1+l3>l2) && (l1>0 && l2>0 && l3>0))
 	{
-		return 0;
+		return true;
+		printf("It's a triangle");
     }
 	else
 	{
-		return 1;
+		return false;
+		printf("It's not a triangle");
 	}
 
 }
