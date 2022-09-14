f0e2be906e0e786a72081eadd6573741ca81f29f
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-14 07:44:58 +0000

    Wed, Sep 14, 2022, 7:44 AM +00:00

--- triangle.c
+++ triangle.c
@@ -17,12 +17,10 @@ bool valid_triangle(int l1, int l2, int l3)
 
 	if (l1+l2>l3 && l2+l3>l1 && l1+l3>l2 && l1>0 && l2>0 && l3>0)
 	{
-		return true;
 		printf("It's a triangle");
     }
 	else
 	{
-		return false;
 		printf("It's not a triangle");
 	}
 
