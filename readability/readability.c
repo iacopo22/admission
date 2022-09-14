569b22e29c06868d71e4fe11a28ebd211d1becd6
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-14 18:16:51 +0000

    Wed, Sep 14, 2022, 6:16 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,6 +6,7 @@ int main(void)
 	string text = get_string("Text: ");
 	printf("Text: %s\n", text);
 
-	int x == int count_letters(string text);
+	int count_letters(string text);
+	printf("Number of letters: %i", int count_letters);
 
 }
