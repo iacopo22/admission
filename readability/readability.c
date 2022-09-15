9bc51c889ddd8c4bdd840c30ebe84ae8439880a6
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-15 16:47:40 +0000

    Thu, Sep 15, 2022, 4:47 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -48,7 +48,7 @@ int count_words(string text)
 	int words = 0;
 	for (int j = 0, n = strlen(text); j < n; j++)
 	{
-		if (isalpha(text[0]) && text[(strlen(text))] == )
+		if (isalpha(text[0]))
 		{
 			if (text[j] == 32)
 			{
