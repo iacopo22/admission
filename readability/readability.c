1239e313f8ce49c0f54e629aa85f816362c22179
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-15 05:18:45 +0000

    Thu, Sep 15, 2022, 5:18 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,7 +4,7 @@
 //I declare the function
 int count_letters(string text);
 
-int main(arg c, argv[])
+int main(int arg c, string argv[])
 {
 	//I want the user to prompt a string, not numbers or symbols
 	do
