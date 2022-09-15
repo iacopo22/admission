e14cd47e1e0c9abd8facc2ac3c11ef113e188f3f
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-15 08:17:31 +0000

    Thu, Sep 15, 2022, 8:17 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -20,14 +20,10 @@ int main(void)
 }
 
 //I define the function
-int count_letters(int n_words, string text[])
+int count_letters(string text[])
 {
 	for (int i = 0; i < n_words; i++)
 	{
-		for (int j = 0; j < strlen(text[i]); j++)
-		{
-			printf("%i", text[i][j]);
-		}
-		printf("\n");
+
 	}
 }
