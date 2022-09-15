cf995dcd4f3f0a8ff5120c40236b09277bcde1ad
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-15 05:34:59 +0000

    Thu, Sep 15, 2022, 5:34 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,14 +4,10 @@
 //I declare the function
 int count_letters(string text);
 
-int main(int arg c, string argv[])
+int main(int argc, string argv[])
 {
 	//I want the user to prompt a string, not numbers or symbols
-	do
-	{
-		string text = get_string("Text: ");
-	}
-	while(0 < stringargv[] < 64);
+	string text = get_string("Text: ");
 
 	//Here the program prints the text
 	printf("Text: %s\n", text);
@@ -25,5 +21,5 @@ int main(int arg c, string argv[])
 //I define the function
 int count_letters(string text)
 {
-
+	strlen
 }
