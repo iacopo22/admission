0ff8051288260e93ec5c48ad4c9d4d6fa396787a
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-15 06:31:01 +0000

    Thu, Sep 15, 2022, 6:31 AM +00:00

---
+++ ff.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	string s = get_string("String: ");
+    for (int i = 0; i < strlen(s); i++)
+    {
+        int c = (int) s[i];
+        printf("%c %i\n", s[i], c);
+    }
+}
