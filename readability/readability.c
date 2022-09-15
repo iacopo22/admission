df6cd2f32ab186821d2bfb76ca99cb258d1b8875
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-15 14:57:00 +0000

    Thu, Sep 15, 2022, 2:57 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -28,7 +28,7 @@ int count_letters(string text)
     for (int i = 0, n = strlen(text); i < n; i++)
     {
 		//Conditions to avod numbers, symbols and spaces
-		if ((0 < (int)text[i] < 64) || ((int)text[i] > 123))
+		if (((int)text[i] > 123) || (0 < (int)text[i] && (int)text[i] < 64))
 		{
 			break;
 		}
