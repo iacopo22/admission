b62397e47831d83892cd6a9be83251be012e5505
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-15 06:54:30 +0000

    Thu, Sep 15, 2022, 6:54 AM +00:00

--- ff.c
+++ ff.c
@@ -8,7 +8,7 @@ int main(int argc, string argv[])
     {
         for (int j = 0, n = strlen(argv[i]); j < n; j++)
         {
-            printf("%c\n", argv[j]);
+            printf("%c\n", argv[i][j]);
         }
         printf("\n");
     }
