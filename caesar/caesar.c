b6b516ebc06f621c069d8c2265508f83a706fca3
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 10:44:48 +0000

    Fri, Sep 16, 2022, 10:44 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -16,17 +16,17 @@ int main(int argc, string argv[])
 	return 1;
 
 	//The input has to be greater and equal to 0 and smaller than 10
-	bool x = only_digits(argv[1]);
+	only_digits(argv[1]);
 
-	if (x == false)
-	{
-		printf("Usage: ./caesar key\n");
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
+
+
+
+
+
+	//If it's not a decimal point the program prints an error
+
+
+	//If the k is greater than 26, the program should convert the letter in another letter following alphabetical order (check ASCII)
 
 
 }
@@ -36,9 +36,14 @@ bool only_digits(string s)
 {
 	for (int i = 0, n = strlen(s); i < n; i++)
 	{
-		if (!isdigit(s[i]))
+		if (isdigit(s[i]))
 		{
-			return false;
+
+		}
+		else
+		{
+			printf("Usage: ./caesar key\n");
+			return 0;
 		}
 	}
 	return true;
