3d03e4b28b86df1332c29914c55030f39acf5202
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 06:19:51 +0000

    Fri, Sep 16, 2022, 6:19 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+	//The program receive a command line from the user to declare the Key (positive integer)
+
+	//If there aren't input or it there are more then one, the program prints an error
+
+
+	//If it's not a decimal point the program prints an error
+
+
+	//If the k is greater than 26, the program should convert the letter in another letter following alphabetical order (check ASCII)
+
 
 }
