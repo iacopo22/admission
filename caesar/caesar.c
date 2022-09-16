e474b633efb555dccf767cdf110e8672246615d1
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 06:28:26 +0000

    Fri, Sep 16, 2022, 6:28 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,6 +4,10 @@
 int main(int argc, string argv[])
 {
 	//The program receive a command line from the user to declare the Key (positive integer)
+	if (string arv[1] == " " && string arv[2] != "\0")
+	{
+
+	}
 
 	//If there aren't input or it there are more then one, the program prints an error
 
