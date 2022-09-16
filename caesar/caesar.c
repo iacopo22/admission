300c3c4ce8db2301091d98cf29a0597624ac3148
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 07:07:42 +0000

    Fri, Sep 16, 2022, 7:07 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,16 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool only_digits(string input);
+
 int main(int argc, string argv[])
 {
-	//If there aren't input or it there are more then one, the program prints an error
+	//If there aren't input or if there are more then one, the program prints an error
 	if (argv[1] == "\0" || argv[2] != NULL)
 	{
 		printf("Usage: ./caesar key\n");
 	}
 	return 1;
 
-	//If there aren't input or it there are more then one, the program prints an error
+	//The input has to be greater and equal to 0 and smaller than 10
+	only_digits(input);
 
 
 	//If it's not a decimal point the program prints an error
@@ -18,5 +21,11 @@ int main(int argc, string argv[])
 
 	//If the k is greater than 26, the program should convert the letter in another letter following alphabetical order (check ASCII)
 
+
+}
+
+
+bool only_digits(string input)
+{
 
 }
