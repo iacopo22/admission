9d590ecd77c37da0e70532095ab99d617d503a01
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 09:07:45 +0000

    Fri, Sep 16, 2022, 9:07 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -17,7 +17,6 @@ int main(int argc, string argv[])
 
 	//The input has to be greater and equal to 0 and smaller than 10
 	bool x = only_digits(argv[1]);
-	printf("%bool", x);
 
 
 	//If it's not a decimal point the program prints an error
