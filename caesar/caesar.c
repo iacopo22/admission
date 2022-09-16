65569f204adf4b395345b805b1f84c6401acbf83
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 11:16:00 +0000

    Fri, Sep 16, 2022, 11:16 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -21,11 +21,8 @@ int main(int argc, string argv[])
 	only_digits(argv[1]);
 
 
-	for (int j = 0, n = strlen(argv[1]); j < n; j++)
-	{
-	int number(string argv[j]);
-	printf("Number: %i\n", number)
-	}
+	printf("This is CS%i\n", atoi(argv[1]));
+
 
 
 
