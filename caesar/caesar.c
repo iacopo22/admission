21a044c347f4f40c24821b82fdcb9bc2cfa74002
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 06:33:16 +0000

    Fri, Sep 16, 2022, 6:33 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,11 +4,11 @@
 int main(int argc, string argv[])
 {
 	//The program receive a command line from the user to declare the Key (positive integer)
-	if (argv[1] == "0" || argv[2] != "\0")
+	if (argv[1] == "\0" || argv[2] != "\0")
 	{
 		Printf("Usage: ./caesar key\n");
-		return 1;
 	}
+	return 1;
 
 	//If there aren't input or it there are more then one, the program prints an error
 
