3f0d5fd9ae9d9bcf8f9b8650c47a2e707d23fe4a
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-16 07:00:04 +0000

    Fri, Sep 16, 2022, 7:00 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,9 +4,9 @@
 int main(int argc, string argv[])
 {
 	//If there aren't input or it there are more then one, the program prints an error
-	if (argv[1] == '\0' || argv[2] != '\0')
+	if (argv[1] == NULL || argv[2] != NULL)
 	{
-		Printf("Usage: ./caesar key\n");
+		printf("Usage: ./caesar key\n");
 	}
 	return 1;
 
