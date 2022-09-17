e1814f52374b23cba727639949e0578c6bc95a5b
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-17 11:14:39 +0000

    Sat, Sep 17, 2022, 11:14 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,14 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+bool chars_argv (string text);
 
 int main(int argc, string argv[])
 {
+	//Two argument lines
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return 1;
 	}
+	//Key must contain 26 characters
 	int j = 0;
 	for (int i = 0, n = strlen(argv[1]); i < n; i++)
 	{
@@ -18,4 +23,19 @@ int main(int argc, string argv[])
 	{
 		printf("Key must contain 26 characters\n");
 	}
+
+	//Key must only contain alphabetical characters
+	chars_argv (argv[1]);
+}
+
+bool chars_argv (string text)
+{
+	for (int z = 0, n = strlen(text); z < n; z++)
+	{
+		if (!isalpha(text[z]))
+		{
+			printf("Key must only contain alphabetical characters\n");
+		}
+	}
+	return 1;
 }
