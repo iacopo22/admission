8e07c5b014dbf457db496c3008cef98ce8f8cad4
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-18 16:37:59 +0000

    Sun, Sep 18, 2022, 4:37 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,81 +9,81 @@ char cyphertext (string k, char plain);
 
 int main(int argc, string argv[])
 {
-	//Two argument lines
-	if (argc != 2)
-	{
-		printf("Error: the program requires two argument lines\n");
-		return 1;
-	}
+    //Two argument lines
+    if (argc != 2)
+    {
+        printf("Error: the program requires two argument lines\n");
+        return 1;
+    }
 
-	//Key must contain 26 characters
-	int j = 0;
-	for (int i = 0, n = strlen(argv[1]); i < n; i++)
-	{
-		j++;
-	}
-	if (j != 26)
-	{
-		printf("Key must contain 26 characters\n");
-		return 1;
-	}
+    //Key must contain 26 characters
+    int j = 0;
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        j++;
+    }
+    if (j != 26)
+    {
+        printf("Key must contain 26 characters\n");
+        return 1;
+    }
 
-	//Key must only contain alphabetical characters
-	bool number = alphabetical_chars (argv[1]);
+    //Key must only contain alphabetical characters
+    bool number = alphabetical_chars(argv[1]);
 
-	if (!alphabetical_chars (argv[1]))
-	{
-		return 1;
-	}
+    if (!alphabetical_chars(argv[1]))
+    {
+        return 1;
+    }
 
-	//Key can't contain two equal characters
-	bool same = same_chars (argv[1]);
+    //Key can't contain two equal characters
+    bool same = same_chars(argv[1]);
 
-	if (!same_chars (argv[1]))
-	{
-		return 1;
-	}
+    if (!same_chars(argv[1]))
+    {
+        return 1;
+    }
 
-	//Prompt the user for the plaintext
-	string plaintext = get_string("plaintext: ");
+    //Prompt the user for the plaintext
+    string plaintext = get_string("plaintext: ");
 
-	//Encipher the plaintext to obtain the ciphertext
-	string key = argv[1];
+    //Encipher the plaintext to obtain the ciphertext
+    string key = argv[1];
 
-	printf("ciphertext: ");
+    printf("ciphertext: ");
 
-	for (int x = 0, n = strlen(plaintext); x < n; x++)
-	{
-		char letter_ciphered = cyphertext (key, plaintext[x]);
-		printf("%c", letter_ciphered);
-	}
+    for (int x = 0, n = strlen(plaintext); x < n; x++)
+    {
+        char letter_ciphered = cyphertext(key, plaintext[x]);
+        printf("%c", letter_ciphered);
+    }
 
     printf("\n");
     return 0;
 }
 
-bool alphabetical_chars (string text)
+bool alphabetical_chars(string text)
 {
-	for (int z = 0; z < strlen(text); z++)
-	{
-		if (!isalpha(text[z]))
-		{
-			printf("Key must only contain alphabetical characters\n");
-			return false;
-		}
-	}
+    for (int z = 0; z < strlen(text); z++)
+    {
+        if (!isalpha(text[z]))
+        {
+            printf("Key must only contain alphabetical characters\n");
+            return false;
+        }
+    }
     return true;
 }
 
-bool same_chars (string key)
+bool same_chars(string key)
 {
-	for (int i = 0, n = strlen(key); i < n; i++)
-	{
-		if ((key[i+1]) == (key[i]))
-		{
-			printf("Key cannot have two equal characters\n");
-			return false;
-		}
+    for (int i = 0, n = strlen(key); i < n; i++)
+    {
+        if ((key[i + 1]) == (key[i]))
+        {
+            printf("Key cannot have two equal characters\n");
+            return false;
+        }
     }
     return true;
 }
