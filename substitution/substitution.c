c6d7b72ec6029fc9504b74a79880804f8e19fbb4
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-18 08:20:25 +0000

    Sun, Sep 18, 2022, 8:20 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -52,6 +52,11 @@ int main(int argc, string argv[])
 	for (int x = 0, n = strlen(plaintext); x < n; x++)
 	{
 		printf("Each character of the plaintext: %c\n", plaintext[x]);
+		if (isupper(plaintext[x]))
+		{
+			
+		}
+
 	}
 
 
