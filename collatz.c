18daff69e7d017b8b35482df2346d8d522d90b8d
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-19 08:05:05 +0000

    Mon, Sep 19, 2022, 8:05 AM +00:00

---
+++ collatz.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int counter = 0;
+    int collatz(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else if (n % 2 == 0)
+        {
+            counter++;
+            return n/2;
+        }
+        else if (n % 2 != 0)
+        {
+            counter++;
+            return (3 * n) + 1;
+        }
+    }
+
+}
