e8f70016914781bfabefd8884789ad4b7016dde8
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-19 08:57:46 +0000

    Mon, Sep 19, 2022, 8:57 AM +00:00

--- collatz.c
+++ collatz.c
@@ -6,23 +6,7 @@
 
 
 
-    int collatz(int n)
-    {
-        if (n == 1)
-        {
-            return 1;
-        }
-        else if (n % 2 == 0)
-        {
-            return n/2;
-        }
-        else if (n % 2 != 0)
-        {
-            return (3 * n) + 1;
-        }
-        return n;
-    }
-
+int collatz(int n);
 
 
 int main(void)
@@ -37,4 +21,21 @@ int main(void)
     printf("Number of times: %i", counter);
 
     return 0;
+}
+
+int collatz(int n)
+{
+     if (n == 1)
+     {
+           return 1;
+     }
+     else if (n % 2 == 0)
+     {
+          return n/2;
+     }
+     else if (n % 2 != 0)
+     {
+          return (3 * n) + 1;
+     }
+     return n;
 }
