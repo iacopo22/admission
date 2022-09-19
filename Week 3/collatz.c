14f02027aa6a7659f3769a10a64ab608d4818723
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-19 08:03:23 +0000

    Mon, Sep 19, 2022, 8:03 AM +00:00

--- Week 3/collatz.c
+++ Week 3/collatz.c
@@ -7,6 +7,23 @@
 
 int main(void)
 {
-    
+    int counter = 0;
+    int collatz(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else if (n % 2 == 0)
+        {
+            counter++;
+            return n/2;
+        }
+        else if (n % 2 != 0)
+        {
+            counter++;
+            return (3 * n) + 1;
+        }
+    }
 
 }
