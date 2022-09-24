459f90c9b743e82e5629c398efa4913869c6c684
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-24 15:27:56 +0000

    Sat, Sep 24, 2022, 3:27 PM +00:00

--- Week 2/substitution/substitution.c
+++ Week 2/substitution/substitution.c
@@ -80,7 +80,7 @@ bool same_chars(string key)
 {
     for (int i = 0, n = strlen(key); i < n; i++)
     {
-        for (int j = 0; j < n; j += 1 / 26)
+        for (int j = 0; j < n; j += (1 / 26))
         {
             if ((key[j]) == (key[i]))
             {
