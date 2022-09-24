b9484171ae775a467de77bcd15540bb091099908
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-24 11:29:51 +0000

    Sat, Sep 24, 2022, 11:29 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,10 +71,6 @@ bool vote(string name)
             candidates[x].votes++;
             return true;
         }
-        else if (!(strcmp(name, candidates[x].name) == 0))
-        {
-            return false;
-        }
     }
-    return true;
+    return false;
 }
