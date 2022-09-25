98efae6f9b3d65f8a1408045791454fa5dc4ea10
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-25 15:28:05 +0000

    Sun, Sep 25, 2022, 3:28 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -88,8 +88,8 @@ void print_winner(void)
         if (candidates[x].votes > candidates[candidate_count - 1].votes)
         {
             candidate_count--;
-            printf("%i\n", candidates[x].name);
+            printf("%s\n", candidates[x].name);
         }
     }
     return;
-}                    4 1 3
+}                    
