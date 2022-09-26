446ccf7a81d3cfa3f7da27e06b62d158ff17ff52
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-26 09:02:49 +0000

    Mon, Sep 26, 2022, 9:02 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -149,10 +149,6 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for ( int j = 0; j < candidate_count; j++)
-    {
-        printf("Number of votes: %i\n", candidates[j].votes++);
-    }
     return;
 }
 
