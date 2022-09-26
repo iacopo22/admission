341facb949be854be0d0dee62fdfe9d2eafced0d
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-26 07:27:31 +0000

    Mon, Sep 26, 2022, 7:27 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,7 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    fpr (int i = 0; )
+    fpr (int i = 0; i < )
     return false;
 }
 
