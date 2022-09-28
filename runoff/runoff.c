214fdd34d0731519f50173b73aa6151a9d19ceee
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-28 06:32:01 +0000

    Wed, Sep 28, 2022, 6:32 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -140,9 +140,8 @@ bool vote(int voter, int rank, string name)
                 //If they match I add 1 to
                 if (strcmp(name, candidates[i].name) == 0)
                 {
-                    printf("Voter: %i\n", voter);
-                    printf("Rank: %i\n", rank);
-                    preferences[voter][rank]++;
+                    preferences[voter][rank] == i;
+                    
                     printf("%i\n", preferences[voter][rank]);
 
                     candidates[i].votes++;
