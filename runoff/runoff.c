7139e4748ed5b16c516996f28d6ab88fa1f5a840
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-09-28 09:24:35 +0000

    Wed, Sep 28, 2022, 9:24 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,19 +145,19 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    
+
     for (int voter = 0; voter < voter_count; voter++)
     {
         for (int rank = 0; rank < candidate_count; rank++)
         {
-            if (candidates[preferences[voter][rank]].eliminated == false)
+            if (candidates[preferences[voter][rank]].eliminated == true)
+            {
+            
+            }
+            else if (candidates[preferences[voter][rank]].eliminated == false)
             {
                 candidates[preferences[voter][rank]].votes++;
                 break;
-            }
-            else if (candidates[preferences[voter][rank]].eliminated == true)
-            {
-                continue;
             }
 
         }
