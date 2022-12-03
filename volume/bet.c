dc4483b0f6a0b2ce13b0ea5625b15d36608f5a5e
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2022-12-03 12:27:15 +0000

    Sat, Dec 3, 2022, 12:27 PM +00:00

---
+++ volume/bet.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    int scom1;
+    int scom2;
+    int scom3;
+    int scom4;
+    int scom5;
+    int scom6;
+    int scom7;
+    int scom8;
+
+    scom1 * 3.2 - scom1 - scom2 - scom3 - scom4 - scom5 - scom6  - scom7 - scom8 = 0;
+    scom2 * 6 - scom1 - scom2 - scom3 - scom4 - scom5 - scom6  - scom7 - scom8 = 0;
+    scom3 * 6.5 - scom1 - scom2 - scom3 - scom4 - scom5 - scom6 - scom7 - scom8 = 0;
+    scom4 * 7 - scom1 - scom2 - scom3 - scom4 - scom5 - scom6 - scom7 - scom8  = 0;
+    scom5 * 10 - scom1 - scom2 - scom3 - scom4 - scom5 - scom6 - scom7 - scom8 = 0;
+    scom6 * 13 - scom1 - scom2 - scom3 - scom4 - scom5 - scom6 - scom7 - scom8 = 0;
+    scom7 * 21 - scom1 - scom2 - scom3 - scom4 - scom5 - scom6 - scom7 - scom8 = 0;
+    scom8 * 34  - scom1 - scom2 - scom3 - scom4 - scom5 - scom6 - scom7 - scom8 = 0;
+
+}
+
