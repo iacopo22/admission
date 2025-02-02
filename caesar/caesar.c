98f152215a0996ec180a9add36c5bc510413efe8
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-02-02 14:18:40 +0000

    Sun, Feb 2, 2025, 3:18 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 
+bool is_int(string s);
 
 int main(int argc, string argv[])
 {
@@ -13,10 +14,25 @@ int main(int argc, string argv[])
         return 1;
     }
 
+    bool integer = is_int(argv[1]);
+
+    return 1;
 }
 
 
 bool is_int(string s)
 {
-    
+    int length = strlen(s);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (s[i] >= 0 && s[i] <= 9)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
