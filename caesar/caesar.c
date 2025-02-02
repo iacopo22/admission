e1d123217699c0e0099a657a39d6900148e2e13d
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-02-02 14:24:54 +0000

    Sun, Feb 2, 2025, 3:24 PM +01:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -33,14 +33,12 @@ bool is_int(string s)
 
     for (int i = 0; i < length; i++)
     {
-        if (s[i] >= 0 && s[i] <= 9)
+        if (s[i] >= 48 && s[i] <= 57)
         {
-            printf("%i\n", s[i]);
             x = true;
         }
         else
         {
-            printf("%i\n", s[i]);
             x = false;
         }
     }
