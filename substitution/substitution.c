c3362902c943ee82022a3438582d97950affe3ab
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-02-02 16:01:57 +0000

    Sun, Feb 2, 2025, 5:01 PM +01:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -7,5 +7,15 @@
 
 int main(int argc, string argv[])
 {
-    printf("ciao");
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    if (strlen(argv[1] != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
 }
