795aeba2b3b329ca076c13f88fe3bd26015f46f9
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-02-03 18:39:51 +0000

    Mon, Feb 3, 2025, 7:39 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -141,7 +141,18 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == true)
+        {
+            return;
+        }
+        else if (candidates[i].eliminated == false && candidate[i])
+        {
+
+        }
+
+    }
     return;
 }
 
