3dd726f5786f9b0a97c1dbef2fbc538f3bfa7555
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-02-03 19:33:00 +0000

    Mon, Feb 3, 2025, 8:33 PM +01:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -159,7 +159,14 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > (voter_count/2))
+        {
+            priintf("Winner is %s\n", candidates[i].names);
+            return true;
+        }
+    }
     return false;
 }
 
