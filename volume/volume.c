508e0c3baf95f6629b115dd8842c69770f137f48
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-02-13 20:00:26 +0000

    Thu, Feb 13, 2025, 9:00 PM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     BYTE_2 samples;
 
-    while (feof(input) != 0)
+    while (feof(input) == 0)
     {
         fread(&samples, sizeof(samples), 1, input);
         samples *= factor;
