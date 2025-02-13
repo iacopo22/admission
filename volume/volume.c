dbb14a746e9cbbd29417d330d5b028056cfda20d
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-02-13 12:01:05 +0000

    Thu, Feb 13, 2025, 1:01 PM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -7,7 +7,8 @@
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
-typedef uint16_t BYTE;
+typedef uint8_t BYTE;
+typedef uint16_t BYTE_2;
 
 
 int main(int argc, char *argv[])
@@ -38,12 +39,13 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
+    BYTE b;
 
-    fread(header, HEADER_SIZE, 1, input);
-    fwrite(header, HEADER_SIZE, 1, output);
+    fread(header, sizeof(b), HEADER_SIZE, input);
+    fwrite(header, sizeof(b), HEADER_SIZE, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    BYTE b;
+
 
     printf("%ld\n", ftell(input));
     printf("%ld\n", ftell(output));
