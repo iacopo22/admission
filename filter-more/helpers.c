9d5468fe9690c66f1653a7bade5c5ac39844aa1a
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-05 00:19:41 +0000

    Wed, Mar 5, 2025, 1:19 AM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,14 +1,45 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop over every pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Average of the RGB of the pixel
+            int average =
+                round((image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3.0);
+
+            // Assign the average to the RGB values of the pixel
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+        }
+    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            int Red = image[i][j].rgbtRed;
+            int Blue = image[i][j].rgbtBlue;
+            int Green = image[i][j].rgbtGreen;
+            image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
+            image[i][j].rgbtBlue = image[i][width - j - 1].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][width - j - 1].rgbtGreen;
+            image[i][width - j - 1].rgbtRed = Red;
+            image[i][width - j - 1].rgbtBlue = Blue;
+            image[i][width - j - 1].rgbtGreen = Green;
+        }
+    }
     return;
 }
 
