5e56947b0bd96b529e0d9fac5d3cf7b029a69e01
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-10 18:26:37 +0000

    Mon, Mar 10, 2025, 7:26 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < ceil(end / 512); i++)
     {
-        fread(block, 1, 1, memory);
+        fread(block, 1, 512, memory);
 
         int counter = 0;
 
@@ -40,7 +40,6 @@ int main(int argc, char *argv[])
         {
             if (counter == 0)
             {
-                fread(block, 1, 512, memory);
                 fwrite(block, 1, 512, jpg);
 
                 counter++;
