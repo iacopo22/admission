815260bdd2e4846479f4b8c042640612a6cd82a5
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-10 11:17:14 +0000

    Mon, Mar 10, 2025, 12:17 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     {
         fread(&block, sizeof(block), 512, memory);
 
-        if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] && 0xf0) == 0xe0)
+        if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] & 0xf0) == 0xe0)
         {
 
         }
