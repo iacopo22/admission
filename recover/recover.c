d2bf4c27822c4ac6940b82d4739ab40eb6fe3e9e
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-10 18:22:39 +0000

    Mon, Mar 10, 2025, 7:22 PM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
     printf("%x\n", block[1]);
     printf("%x\n", block[2]);
     printf("%x\n", block[3]);
-    printf("%x\n", block[]);
+    printf("%x\n", block[4]);
     fclose(jpg);
 
 }
