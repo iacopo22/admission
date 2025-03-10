c3d365d588338954d5885462f8b47e620a769beb
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-10 10:35:38 +0000

    Mon, Mar 10, 2025, 11:35 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("The program accepts exactly one command line argument, i.e. the name of a forensic image");
+        return 1;
+    }
+
 }
