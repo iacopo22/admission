db371a7d4b067779cbcc7f829708f2f51e14766f
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-10 10:38:38 +0000

    Mon, Mar 10, 2025, 11:38 AM +01:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,7 +5,14 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("The program accepts exactly one command line argument, i.e. the name of a forensic image");
+        printf("The program accepts exactly one command line argument, i.e. the name of a forensic image.\n");
+        return 1;
+    }
+
+    FILE *memory = fopen(argv[1], "r");
+    if (memory == NULL)
+    {
+        printf("Could not open the forensic image.\n");
         return 1;
     }
 
