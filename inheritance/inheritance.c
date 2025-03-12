6666eaa1859566d92592669b456d40a896be4100
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-12 10:52:45 +0000

    Wed, Mar 12, 2025, 11:52 AM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -5,89 +5,127 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Implements a list of numbers as a binary search tree
-
-#include <stdio.h>
-#include <stdlib.h>
-
-// Represents a node
-typedef struct node
+// Each person has two parents and two alleles
+typedef struct person
 {
-    int number;
-    struct node *left;
-    struct node *right;
-}
-node;
+    struct person *parents[2];
+    char alleles[2];
+} person;
 
-void free_tree(node *root);
-void print_tree(node *root);
+const int GENERATIONS = 3;
+const int INDENT_LENGTH = 4;
+
+person *create_family(int generations);
+void print_family(person *p, int generation);
+void free_family(person *p);
+char random_allele();
 
 int main(void)
 {
-    // Tree of size 0
-    node *tree = NULL;
+    // Seed random number generator
+    srandom(time(0));
 
-    // Add number to list
-    node *n = malloc(sizeof(node));
-    if (n == NULL)
-    {
-        return 1;
-    }
-    n->number = 2;
-    n->left = NULL;
-    n->right = NULL;
-    tree = n;
+    // Create a new family with three generations
+    person *p = create_family(GENERATIONS);
 
-    // Add number to list
-    n = malloc(sizeof(node));
-    if (n == NULL)
-    {
-        free_tree(tree);
-        return 1;
-    }
-    n->number = 1;
-    n->left = NULL;
-    n->right = NULL;
-    tree->left = n;
+    // Print family tree of blood types
+    print_family(p, 0);
 
-    // Add number to list
-    n = malloc(sizeof(node));
-    if (n == NULL)
-    {
-        free_tree(tree);
-        return 1;
-    }
-    n->number = 3;
-    n->left = NULL;
-    n->right = NULL;
-    tree->right = n;
-
-    // Print tree
-    print_tree(tree);
-
-    // Free tree
-    free_tree(tree);
-    return 0;
+    // Free memory
+    free_family(p);
 }
 
-void free_tree(node *root)
+// Create a new individual with `generations`
+person *create_family(int generations)
 {
-    if (root == NULL)
+    // TODO: Allocate memory for new person
+
+    // If there are still generations left to create
+    if (generations > 1)
+    {
+        // Create two new parents for current person by recursively calling create_family
+        person *parent0 = create_family(generations - 1);
+        person *parent1 = create_family(generations - 1);
+
+        // TODO: Set parent pointers for current person
+
+        // TODO: Randomly assign current person's alleles based on the alleles of their parents
+    }
+
+    // If there are no generations left to create
+    else
+    {
+        // TODO: Set parent pointers to NULL
+
+        // TODO: Randomly assign alleles
+    }
+
+    // TODO: Return newly created person
+    return NULL;
+}
+
+// Free `p` and all ancestors of `p`.
+void free_family(person *p)
+{
+    // TODO: Handle base case
+
+    // TODO: Free parents recursively
+
+    // TODO: Free child
+}
+
+// Print each family member and their alleles.
+void print_family(person *p, int generation)
+{
+    // Handle base case
+    if (p == NULL)
     {
         return;
     }
-    free_tree(root->left);
-    free_tree(root->right);
-    free(root);
+
+    // Print indentation
+    for (int i = 0; i < generation * INDENT_LENGTH; i++)
+    {
+        printf(" ");
+    }
+
+    // Print person
+    if (generation == 0)
+    {
+        printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+    }
+    else if (generation == 1)
+    {
+        printf("Parent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+    }
+    else
+    {
+        for (int i = 0; i < generation - 2; i++)
+        {
+            printf("Great-");
+        }
+        printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
+    }
+
+    // Print parents of current generation
+    print_family(p->parents[0], generation + 1);
+    print_family(p->parents[1], generation + 1);
 }
 
-void print_tree(node *root)
+// Randomly chooses a blood type allele.
+char random_allele()
 {
-    if (root == NULL)
+    int r = random() % 3;
+    if (r == 0)
     {
-        return;
+        return 'A';
     }
-    print_tree(root->left);
-    printf("%i\n", root->number);
-    print_tree(root->right);
+    else if (r == 1)
+    {
+        return 'B';
+    }
+    else
+    {
+        return 'O';
+    }
 }
