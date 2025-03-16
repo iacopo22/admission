a5fcec0b970141031312d8a5ca7ad9fce3af97f3
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-16 21:12:40 +0000

    Sun, Mar 16, 2025, 10:12 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -112,7 +112,7 @@ unsigned int hash(const char *word)
     // Sort based on the first two letters
     if(strlen(word) < 2)
     {
-        return toupper(word[0]) - 'A' + rndm;
+        return toupper(word[0]) - 'A' + (random() / ((double) RAND_MAX + 1)) * 26;
     }
     else
     {
