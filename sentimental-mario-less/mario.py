14a1acd0c156f49ab14eb27d5212c3e5c207a360
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-18 09:57:11 +0000

    Tue, Mar 18, 2025, 10:57 AM +01:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -5,13 +5,19 @@ height = get_int("Height of the half-pyramid: ");
 while (height > 8 or height < 1):
     height = get_int("Height of the half-pyramid: ");
 
-x = 
+x = 0
+y = 0
 for _ in range(height):
     for __ in range(height - 1 - x):
         print(" ", end="")
     for ___ in range(y):
         print("#", end="")
+    # the last block and then \n
     print("#")
+    # Increase the number of blocks and decrease
+    # the number of spaces
+    x += 1
+    y += 1
 
 
 
