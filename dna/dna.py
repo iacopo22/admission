9958c15219a0273ad82d656156f61e68f684c5fa
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-19 09:59:39 +0000

    Wed, Mar 19, 2025, 10:59 AM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -28,7 +28,7 @@ def main():
         for row in reader_:
             DNA.append(row)
 
-    print(f"{reader}")
+    print(f"{reader["AGATC"]}")
     # TODO: Find longest match of each STR in DNA sequence
     for i in range(1, len(reader.fieldnames)):
         match = longest_match(DNA, reader.fieldnames[i])
