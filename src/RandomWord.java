b838eaf143ea6d52b5a61ab3c4e9bbd14a767f6a
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-03-22 14:50:51 +0000

    Sat, Mar 22, 2025, 3:50 PM +01:00

---
+++ src/RandomWord.java
@@ -0,0 +1,27 @@
+package lib;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+
+public class RandomWord {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        String word;
+
+        while (!(StdIn.isEmpty()))
+        {
+            String tmp = StdIn.readString();
+
+            if (StdRandom.bernoulli(1 / i))
+            {
+                word = tmp;
+            }
+        }
+
+        StdOut.println(word);
+    }
+
+}
