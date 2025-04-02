7584f2d0cf8218a23b29a44de249db6ff6132d13
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-04-02 13:33:15 +0000

    Wed, Apr 2, 2025, 3:33 PM +02:00

--- test.py
+++ test.py
@@ -1,22 +1,11 @@
 def groupAnagrams(strs):
+        
 
-        dictionary = {}
-
-        for word in strs:
-            string = {}
-            for char in word:
-                string[char] = 1 + string.get(char, 0)
-
-            ##  Hashable type
-            freq = tuple(sorted(string.items()))
-
-            if freq not in dictionary:
-                 dictionary[freq] = [word]
-            else:
-                 dictionary[freq].append(word)
-
-        print(f"{list(dictionary.values())}")
-        return list(dictionary.values())
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
 
 
 
