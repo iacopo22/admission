90bdbdf3020d3425e284f41e1431a605b321c9b6
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-04-02 09:52:47 +0000

    Wed, Apr 2, 2025, 11:52 AM +02:00

--- test.py
+++ test.py
@@ -1,19 +1,11 @@
 
-def twoSum(nums, target):
-
-      dictionary = {}
-
-      for i, n in enumerate(nums):
-            dictionary[n] = i
-
-      for n in enumerate(nums):
-            print(f"{n}")
-            diff = target - n
-            if diff in dictionary and dictionary[diff] != i:
-                return [i, dictionary[diff]]
 
 
 
 
-twoSum([2,7,11,15], 9)
 
+
+
+
+
+strings = ["eat","tea","tan","ate","nat","bat"]
