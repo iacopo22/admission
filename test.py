2a00141518e21986c7a85c7311a59e328bef7c40
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-04-02 13:34:54 +0000

    Wed, Apr 2, 2025, 3:34 PM +02:00

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
+
 def groupAnagrams(strs):
-        
 
         res = defaultdict(list)
         for s in strs:
             sortedS = ''.join(sorted(s))
             res[sortedS].append(s)
+
+        print(f"{list(res.values())}")
         return list(res.values())
 
 
