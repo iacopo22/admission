6d709eedec6e05f77d8bd290d11dfc2fc1d670b9
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-04-02 13:24:40 +0000

    Wed, Apr 2, 2025, 3:24 PM +02:00

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
+def groupAnagrams(strs):
+
+        dictionary = {}
+
+        for word in strs:
+            string = {}
+            for char in word:
+                string[char] = 1 + string.get(char, 0)
+
+            ##  Hashable type
+            freq = tuple(sorted(string.items()))
+
+            if freq not in dictionary:
+                 dictionary[freq] = [word]
+            else:
+                 dictionary[freq].append(word)
+
+        print(f"{list(dictionary.values())}")
+        return list(dictionary.values())
 
 
 
-
-
-
-
-
-
-
-strings = ["eat","tea","tan","ate","nat","bat"]
+strs = ["eat","tea","tan","ate","nat","bat"]
+groupAnagrams(strs)
