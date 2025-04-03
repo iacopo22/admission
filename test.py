c3edde55dd0566ed490112fb9d50e4c7172326d5
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-04-03 10:28:18 +0000

    Thu, Apr 3, 2025, 12:28 PM +02:00

--- test.py
+++ test.py
@@ -9,15 +9,14 @@ def groupAnagrams(nums, k):
             res[num] += 1
 
         print(f"{res}")
-        sorted(res, key=res.get)
+        res = sorted(res, key=res.get)
         print(f"{res}")
 
-        for i in range(k):
-            output.append(res.keys())
+        
 
         print(f"{output}")
 
 
-nums = [1,1,1,2,2,3]
+nums = [3,2,2,1,1,1]
 k = 2
 groupAnagrams(nums, k)
