02db5abc7840a478eea0ca6e07641545d4635dba
iacopo22 <92464945+iacopo22@users.noreply.github.com>
2025-04-14 12:35:16 +0000

    Mon, Apr 14, 2025, 2:35 PM +02:00

--- test2.py
+++ test2.py
@@ -3,23 +3,22 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime
 from zoneinfo import ZoneInfo
-import matplotlib.dates as mdates
-from matplotlib.ticker import FuncFormatter
+
 
 def est_time_formatter(x, pos):
     dt = mdates.num2date(x).astimezone(ZoneInfo("America/New_York"))
     return dt.strftime('%H:%M')
 # Parameters
 ticker = "AAPL"
-start_date = "2025-03-03"
-end_date = "2025-04-04"
-interval = "5m"
+# Define your new date range (March 3 to April 3, 2025)
+start = datetime.date(2025, 1, 15)
+end = datetime.date(2025, 3, 15)
+interval = "1h"
 
 # Download 10-minute interval data
-df = yf.download(ticker, start=start_date, end=end_date, interval=interval)
+df = yf.download(ticker, start=start, end=end, interval=interval)
 
 # Flatten MultiIndex (handle multi-level columns)
 df.columns = df.columns.get_level_values(0)
@@ -27,19 +26,12 @@ df.columns = df.columns.get_level_values(0)
 # Convert the DataFrame index to Eastern Time ("America/New_York")
 df.index = df.index.tz_convert('America/New_York')
 
-trading_start = datetime.time(9, 30)
-trading_end = datetime.time(16, 0)
-df = df.between_time(trading_start, trading_end)
-
 # Drop rows with missing data
 df.dropna(inplace=True)
 
 # Make sure volume isn't zero to avoid division errors
 df = df[df['Volume'] > 0]
 
-# Define your new date range (March 3 to April 3, 2025)
-start = datetime.date(2025, 2, 15)
-end = datetime.date(2025, 3, 15)
 
 # Filter for the date range
 df_range = df[(df.index.date >= start) & (df.index.date <= end)]
